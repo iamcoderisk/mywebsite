1fcaac17578d1330377ca7a1051b29b473e7ad6d
iamcoderisk <ekeminyd@gmail.com>
2021-02-19 17:23:57 +0100

    some stuffs wwere removed

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # app.py
-from flask import Flask, render_template, request
-
-import os
+from flask import Flask, render_template
 import datetime
 app = Flask(__name__)
 
@@ -12,7 +10,7 @@ def index():
     return render_template('index.html',currentDate = now.strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5555, debug=True)
+    app.run()
 
 
 
