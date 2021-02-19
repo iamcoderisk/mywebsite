f1a0e4e5034685157d4cec7e08549f2c84c9d93b
iamcoderisk <ekeminyd@gmail.com>
2021-02-19 17:29:30 +0100

    new stuff

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     now = datetime.datetime.now()
-
     return render_template('index.html',currentDate = now.strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == '__main__':
